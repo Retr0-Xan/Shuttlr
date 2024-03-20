0a69981ace810138fdff4d403066fa86dc7a7725
Retr0-Xan <mayitey69@gmail.com>
2024-03-20 00:43:21 +0000

    took out unneccesary imports

--- shuttlr/lib/pages/driver_page.dart
+++ shuttlr/lib/pages/driver_page.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors, must_be_immutable
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shuttlr/services/auth.dart';
