587ad0dcf808fd248094fcbd1f39226f899f2a77
Retr0-Xan <mayitey69@gmail.com>
2024-05-03 14:26:24 +0000

    -changed app icon -fixed database coordinates -started working on permission handler

--- shuttlr/lib/pages/driver_page.dart
+++ shuttlr/lib/pages/driver_page.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: prefer_const_constructors, must_be_immutable
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:location/location.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:shuttlr/services/auth.dart';
 import 'package:shuttlr/services/database.dart';
 
@@ -19,25 +22,46 @@ class DriverPage extends StatefulWidget {
 }
 
 class _DriverPageState extends State<DriverPage> {
-  @override
-  void initState() {
-    super.initState();
-    getCurrentLocation();
-  }
-
   final AuthService _auth = AuthService();
 
   bool sessionStarted = false;
   LocationData? currentLocation;
+  StreamSubscription<LocationData>? locationSubscription;
 
-  void getCurrentLocation() {
-    Location location = Location();
+  void getCurrentLocation(bool sessionStatus) async {
+    var status = await Permission.photos.status;
+    if (status.isDenied) {
+      print("Access denied");
+      Permission.photos.request();
+    } else {
+      print("Exception occured");
+    }
+    if (sessionStatus == true) {
+      Location location = Location();
+      location.getLocation().then(
+        (location) async {
+          currentLocation = location;
+          await DatabaseService(uid: widget.uid).updateLocation(
+              (currentLocation!.latitude).toString(),
+              (currentLocation!.longitude).toString());
+        },
+      );
+      if (locationSubscription == null) {
+        locationSubscription = location.onLocationChanged.listen((newLocation) {
+          currentLocation = newLocation;
+          setState(() async {
+            await DatabaseService(uid: widget.uid).updateLocation(
+                (currentLocation!.latitude).toString(),
+                (currentLocation!.longitude).toString());
+          });
+        });
+      }
+    } else if (sessionStatus == false) {
+      locationSubscription?.cancel();
+      locationSubscription = null;
 
-    location.getLocation().then(
-      (location) {
-        currentLocation = location;
-      },
-    );
+      await DatabaseService(uid: widget.uid).updateLocation("", "");
+    }
   }
 
   @override
@@ -49,24 +73,24 @@ class _DriverPageState extends State<DriverPage> {
           IconButton(
               icon: Icon(Icons.logout),
               onPressed: () async {
+                DatabaseService(uid: widget.uid).updateLocation("", "");
                 await _auth.signOut();
               })
         ],
       ),
       body: Center(
         child: GestureDetector(
-          onTap: () async {
+          onTap: () {
             setState(() {
               //this bool will help us change the state of the button and perform other functions
               sessionStarted = !sessionStarted;
             });
-            //start tracking and be updating updateLocation method with new data
-            if (sessionStarted) {
-              await DatabaseService(uid: widget.uid).updateLocation(
-                  (currentLocation!.longitude!).toString(),
-                  (currentLocation!.latitude!).toString());
-            } else {
-              await DatabaseService(uid: widget.uid).updateLocation("", "");
+            //start tracking and be updating updateLocation method with new data if session has been started
+            if (sessionStarted == true) {
+              getCurrentLocation(sessionStarted);
+            } else if (sessionStarted == false) {
+              //if session has ended remove coordinates to remove marker from map
+              getCurrentLocation(sessionStarted);
             }
           },
           child: Container(
--- shuttlr/lib/services/database.dart
+++ shuttlr/lib/services/database.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 //this class will be used to create, read, update locations in the firestore
 //we create a database service class which takes in the uid
 //uid is the uid of the current signed in user
@@ -13,12 +12,13 @@ class DatabaseService {
   final CollectionReference locationCollection =
       FirebaseFirestore.instance.collection('locations');
 
-  Future updateLocation(String longitude, String latitude) async {
+  Future updateLocation(String latitude, String longitude) async {
     return await locationCollection.doc(uid).set({
-      'longitude': longitude,
       'latitude': latitude,
+      'longitude': longitude,
     });
   }
+
   //this is a stream of snapshots
   //basically the current instances of the database at a particular moment in time
   Stream<QuerySnapshot> get locations {
