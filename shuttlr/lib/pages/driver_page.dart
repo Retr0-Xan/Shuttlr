89a0f79c2f6410b0441015a3cffc6a6c5309e28c
Retr0-Xan <mayitey69@gmail.com>
2024-06-02 20:43:29 +0000

    -created multiple pages for driver side -using multiple body widget in list approach

--- shuttlr/lib/pages/driver_page.dart
+++ shuttlr/lib/pages/driver_page.dart
@@ -21,7 +21,20 @@ class DriverPage extends StatefulWidget {
   State<DriverPage> createState() => _DriverPageState();
 }
 
+List<Widget> bodyDriver = [
+  Center(
+    child: Text("Home Page"),
+  ),
+  Center(
+    child: Text("Create Session"),
+  ),
+  Center(
+    child: Text("History"),
+  ),
+];
+
 class _DriverPageState extends State<DriverPage> {
+  Widget? body = bodyDriver[0];
   void getPermissions() async {
     var status = await Permission.locationWhenInUse.status;
     if (!status.isGranted) {
@@ -43,7 +56,7 @@ class _DriverPageState extends State<DriverPage> {
   }
 
   final AuthService _auth = AuthService();
-  int _selectedNavBarIndex = 1;
+  int _selectedNavBarIndex = 0;
   bool sessionStarted = false;
   LocationData? currentLocation;
   StreamSubscription<LocationData>? locationSubscription;
@@ -82,102 +95,100 @@ class _DriverPageState extends State<DriverPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'Create',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
-        ],
-        onTap: (value) {
-          if (value == 0) {
-            // Navigator.pushNamed(context, '/home');
-            print(value);
-            setState(() {
-              _selectedNavBarIndex = value;
-            });
-          } else if (value == 1) {
-            // Navigator.pushNamed(context, '/create');
-            print(value);
-            setState(() {
-              _selectedNavBarIndex = value;
-            });
-          } else {
-            // Navigator.pushNamed(context, '/history');
-            print(value);
-            setState(() {
-              _selectedNavBarIndex = value;
-            });
-          }
-        },
-        currentIndex: _selectedNavBarIndex,
-        selectedItemColor: Colors.green,
-      ),
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Driver page'),
-        actions: [
-          IconButton(
-              icon: Icon(Icons.logout),
-              onPressed: () async {
-                locationSubscription?.cancel();
-                locationSubscription = null;
-                updateLocation("", "");
-                await _auth.signOut();
-              })
-        ],
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              //this bool will help us change the state of the button and perform other functions
-              sessionStarted = !sessionStarted;
-            });
-            //start tracking and be updating updateLocation method with new data if session has been started
-            if (sessionStarted == true) {
-              getCurrentLocation(sessionStarted);
-            } else if (sessionStarted == false) {
-              //if session has ended remove coordinates to remove marker from map
-              locationSubscription?.cancel();
-              locationSubscription = null;
-
-              updateLocation("", "");
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Create',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              label: 'History',
+            ),
+          ],
+          onTap: (value) {
+            if (value == 0) {
+              setState(() {
+                body = bodyDriver[value];
+                _selectedNavBarIndex = value;
+              });
+            } else if (value == 1) {
+              setState(() {
+                body = bodyDriver[value];
+                _selectedNavBarIndex = value;
+              });
+            } else {
+              setState(() {
+                body = bodyDriver[value];
+                _selectedNavBarIndex = value;
+              });
             }
           },
-          child: Container(
-            margin: EdgeInsets.only(top: 40, bottom: 10),
-            width: 300,
-            height: 50,
-            decoration: sessionStarted
-                ? BoxDecoration(
-                    borderRadius: BorderRadius.circular(10), color: Colors.red)
-                : BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color.fromARGB(255, 4, 184, 97)),
-            child: Center(
-                child: sessionStarted
-                    ? Text("Stop Session",
-                        style: GoogleFonts.poppins(
-                            fontSize: 15,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500))
-                    : Text("Start Session",
-                        style: GoogleFonts.poppins(
-                            fontSize: 15,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500))),
-          ),
+          currentIndex: _selectedNavBarIndex,
+          selectedItemColor: Colors.green,
         ),
-      ),
-    );
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text('Driver page'),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.logout),
+                onPressed: () async {
+                  locationSubscription?.cancel();
+                  locationSubscription = null;
+                  updateLocation("", "");
+                  await _auth.signOut();
+                })
+          ],
+        ),
+        body: body);
   }
 }
+
+// Center(
+//         child: GestureDetector(
+//           onTap: () {
+//             setState(() {
+//               //this bool will help us change the state of the button and perform other functions
+//               sessionStarted = !sessionStarted;
+//             });
+//             //start tracking and be updating updateLocation method with new data if session has been started
+//             if (sessionStarted == true) {
+//               getCurrentLocation(sessionStarted);
+//             } else if (sessionStarted == false) {
+//               //if session has ended remove coordinates to remove marker from map
+//               locationSubscription?.cancel();
+//               locationSubscription = null;
+
+//               updateLocation("", "");
+//             }
+//           },
+//           child: Container(
+//             margin: EdgeInsets.only(top: 40, bottom: 10),
+//             width: 300,
+//             height: 50,
+//             decoration: sessionStarted
+//                 ? BoxDecoration(
+//                     borderRadius: BorderRadius.circular(10), color: Colors.red)
+//                 : BoxDecoration(
+//                     borderRadius: BorderRadius.circular(10),
+//                     color: Color.fromARGB(255, 4, 184, 97)),
+//             child: Center(
+//                 child: sessionStarted
+//                     ? Text("Stop Session",
+//                         style: GoogleFonts.poppins(
+//                             fontSize: 15,
+//                             color: Colors.white,
+//                             fontWeight: FontWeight.w500))
+//                     : Text("Start Session",
+//                         style: GoogleFonts.poppins(
+//                             fontSize: 15,
+//                             color: Colors.white,
+//                             fontWeight: FontWeight.w500))),
+//           ),
+//         ),
+//       ),
